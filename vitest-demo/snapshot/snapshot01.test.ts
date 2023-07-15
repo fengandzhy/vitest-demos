0f4707d3e011711fce7a0debddda9343c0a4f6df
fengandzhy <fengandzhy@gmail.com>
2023-07-15 15:38:56 +1200

    vitest-snapshot a snapshot demo

--- vitest-demo/snapshot/snapshot01.test.ts
+++ vitest-demo/snapshot/snapshot01.test.ts
@@ -3,7 +3,7 @@ import {expect, it} from 'vitest';
 
 
 it('toUpperCase', () => {
-    const result = 'foobar'.toUpperCase()
+    const result = 'foobar'.toUpperCase();
     expect(result).toMatchSnapshot();
 });
 
